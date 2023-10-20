ab25a7a6bb42fd1a297e1c5cb33656a80c98b8ab
mikan3rd <mikan.no.kikoushi@gmail.com>
2023-10-20 23:36:27 +0900

    chore: add TransactionRepository

--- app/src/modules/transaction.module.ts
+++ app/src/modules/transaction.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TransactionUsecase } from '../usecases/transaction.usecase';
 import { TransactionController } from '../controllers/transaction.controller';
+import { TransactionRepository } from '../repositories/transaction.repository';
 
 @Module({
   imports: [],
   controllers: [TransactionController],
-  providers: [TransactionUsecase],
+  providers: [TransactionRepository, TransactionUsecase],
 })
 export class TransactionModule {}
