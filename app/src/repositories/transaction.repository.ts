627c2e281b0a4045987eb8f91951f783ae0cde0f
mikan3rd <mikan.no.kikoushi@gmail.com>
2023-10-21 00:06:19 +0900

    chore: add validation for amount

--- app/src/repositories/transaction.repository.ts
+++ app/src/repositories/transaction.repository.ts
@@ -10,11 +10,23 @@ export class TransactionRepository {
     userId: number,
     data: Omit<Prisma.transactionsCreateInput, 'users'>,
   ) {
-    return this.prisma.transactions.create({
-      data: {
-        users: { connect: { id: userId } },
-        ...data,
-      },
+    return await this.prisma.$transaction(async (prisma) => {
+      const totalAmount = await prisma.transactions.aggregate({
+        where: { user_id: userId },
+        _sum: { amount: true },
+      });
+
+      if (totalAmount._sum.amount + data.amount > 1000) {
+        // TODO: throw error
+        return;
+      }
+
+      return prisma.transactions.create({
+        data: {
+          users: { connect: { id: userId } },
+          ...data,
+        },
+      });
     });
   }
 }
