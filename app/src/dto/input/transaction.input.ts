26f9aa0d0c90886558f4c9db84fef9e7f777ec56
mikan3rd <mikan.no.kikoushi@gmail.com>
2023-10-21 22:37:36 +0900

    chore: add validation

--- app/src/dto/input/transaction.input.ts
+++ app/src/dto/input/transaction.input.ts
@@ -1,5 +1,15 @@
+import { IsNumber, IsNotEmpty, IsString } from 'class-validator';
+
 export class TransactionInput {
+  @IsNotEmpty()
+  @IsNumber()
   user_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
   amount: number;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
 }
